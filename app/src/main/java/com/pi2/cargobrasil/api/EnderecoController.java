104707b8f9c9f2b6d5539f9c78a4335206b9670a
lipegsouza <lipegsouza3@gmail.com>
2025-06-28 19:27:18 -0300

    adicionando criação de endereço

--- app/src/main/java/com/pi2/cargobrasil/api/EnderecoController.java
+++ app/src/main/java/com/pi2/cargobrasil/api/EnderecoController.java
@@ -1,9 +1,13 @@
 package com.pi2.cargobrasil.api;
 
+import com.pi2.cargobrasil.domain.Endereco;
 import com.pi2.cargobrasil.service.EnderecoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/endereco")
@@ -11,4 +15,18 @@ public class EnderecoController {
 
     @Autowired
     private EnderecoService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Endereco> get(@PathVariable Long id){
+        Optional<Endereco> endereco = service.findById(id);
+
+
+        return endereco.isPresent() ? ResponseEntity.ok(endereco.get()) : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Endereco> post(@RequestBody Endereco endereco){
+        Endereco novoEndereco = service.save(endereco);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novoEndereco);
+    }
 }
--- app/src/main/java/com/pi2/cargobrasil/service/EnderecoService.java
+++ app/src/main/java/com/pi2/cargobrasil/service/EnderecoService.java
@@ -1,11 +1,21 @@
 package com.pi2.cargobrasil.service;
 
+import com.pi2.cargobrasil.domain.Endereco;
 import com.pi2.cargobrasil.repository.EnderecoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class EnderecoService {
     @Autowired
     private EnderecoRepository repository;
+
+    public Optional<Endereco> findById(Long id){
+        return repository.findById(id);
+    }
+    public Endereco save(Endereco endereco){
+        return repository.save(endereco);
+    }
 }
