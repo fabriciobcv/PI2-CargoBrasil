75644ba6da1b51455c6d747eb07810503d701313
lipegsouza <lipegsouza3@gmail.com>
2025-06-28 19:42:27 -0300

    adicionando criação de produto

--- app/src/main/java/com/pi2/cargobrasil/api/ProdutoController.java
+++ app/src/main/java/com/pi2/cargobrasil/api/ProdutoController.java
@@ -1,13 +1,31 @@
 package com.pi2.cargobrasil.api;
 
+import com.pi2.cargobrasil.domain.Produto;
 import com.pi2.cargobrasil.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/produto")
 public class ProdutoController {
     @Autowired
     private ProdutoService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto> get(@PathVariable Long id){
+        Optional<Produto> produto = service.findById(id);
+
+
+        return produto.isPresent() ? ResponseEntity.ok(produto.get()) : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Produto> post(@RequestBody Produto produto){
+        Produto novoProduto = service.save(produto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novoProduto);
+    }
 }
--- app/src/main/java/com/pi2/cargobrasil/service/ProdutoService.java
+++ app/src/main/java/com/pi2/cargobrasil/service/ProdutoService.java
@@ -1,11 +1,21 @@
 package com.pi2.cargobrasil.service;
 
+import com.pi2.cargobrasil.domain.Produto;
 import com.pi2.cargobrasil.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ProdutoService {
     @Autowired
     private ProdutoRepository repository;
+
+    public Optional<Produto> findById(Long id){
+        return repository.findById(id);
+    }
+    public Produto save(Produto produto){
+        return repository.save(produto);
+    }
 }
