925c1608bb2735ce3e731f2c76e69aa3e2f436e9
fabriciobcv <fabriciobcvpessoal@gmail.com>
2025-06-28 10:08:30 -0300

    arquivos iniciais da app

---
+++ app/src/test/java/com/pi2/cargobrasil/CargobrasilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi2.cargobrasil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargobrasilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
