a765de3b39792c413325eb54dec2490ee07d6df1
Fabricio Vasconcelos <fabricio.vasconcelos@bemobi.com>
2025-07-12 18:44:19 -0300

    adicionando get orcamento pelo id do serviço

--- app/src/main/java/com/pi2/cargobrasil/api/OrcamentoController.java
+++ app/src/main/java/com/pi2/cargobrasil/api/OrcamentoController.java
@@ -13,11 +13,7 @@ import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/orcamento")
@@ -51,5 +47,13 @@ public class OrcamentoController {
         }
         
         return !orcamentos.isEmpty() ? ResponseEntity.status(HttpStatus.OK).body(orcamentosDTO) : ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/{servicoId}")
+    public ResponseEntity<OrcamentoDTO> getOrcamentoById(@PathVariable Long servicoId){
+        Optional<Orcamento> orcamento = service.findByServicoId(servicoId);
+        OrcamentoDTO orcamentoDTO = converter.toDto(orcamento.get());
+
+        return orcamento.isPresent() ? ResponseEntity.ok(orcamentoDTO) : ResponseEntity.notFound().build();
     }
 }
--- app/src/main/java/com/pi2/cargobrasil/service/OrcamentoService.java
+++ app/src/main/java/com/pi2/cargobrasil/service/OrcamentoService.java
@@ -2,6 +2,7 @@ package com.pi2.cargobrasil.service;
 
 import com.pi2.cargobrasil.domain.Endereco;
 import com.pi2.cargobrasil.domain.Orcamento;
+import com.pi2.cargobrasil.domain.Servico;
 import com.pi2.cargobrasil.repository.OrcamentoRepository;
 
 import java.util.List;
@@ -15,12 +16,26 @@ public class OrcamentoService {
     @Autowired
     private OrcamentoRepository repository;
 
+    @Autowired
+    private ServicoService servicoService;
+
      public Optional<Orcamento> findById(Long id){
         return repository.findById(id);
     }
 
     public Orcamento save(Orcamento orcamento){
         return repository.save(orcamento);
+    }
+
+    public Optional<Orcamento> findByServicoId(Long servicoId){
+
+        Optional<Servico> servico = servicoService.findById(servicoId);
+
+        Optional<Orcamento> orcamento = repository.findByServico(servico);
+
+
+        return orcamento;
+
     }
 
     public List<Orcamento> list(){
